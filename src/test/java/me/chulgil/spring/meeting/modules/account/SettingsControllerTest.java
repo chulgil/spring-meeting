20e685559ba7ad4fc4c07b2f31a7aa56f2d2829c
chulgil <contact@chulgil.me>
2022-10-22 17:41:53 +0900

    Test: add password

--- src/test/java/me/chulgil/spring/meeting/modules/account/SettingsControllerTest.java
+++ src/test/java/me/chulgil/spring/meeting/modules/account/SettingsControllerTest.java
@@ -11,11 +11,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static me.chulgil.spring.meeting.modules.account.SettingsController.*;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
@@ -29,6 +31,11 @@ class SettingsControllerTest {
     @Autowired
     AccountRepository accountRepository;
 
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    final static String TEST_ACCOUNT = "testAccount";
+    final static String TEST_PASS = "password";
 
     @BeforeEach
     void beforeEach() {
@@ -47,7 +54,7 @@ class SettingsControllerTest {
 
     //@WithUserDetails("cglee") // @BeforeEach보다 먼저 실행됨 (사용할수없음)
     //@WithUserDetails(value="cglee", setupBefore = TestExecutionEvent.TEST_EXECUTION) //(사용할수없음)버그가 있음
-    @WithAccount("testAccount")
+    @WithAccount(TEST_ACCOUNT)
     @DisplayName("프로필 수정하기 - 입력값 정상")
     @Test
     void updateProfile() throws Exception {
@@ -62,5 +69,78 @@ class SettingsControllerTest {
         Account keesun = accountRepository.findByNickname("testAccount");
         assertEquals(bio, keesun.getBio());
     }
+
+    @WithAccount(TEST_ACCOUNT)
+    @DisplayName("프로필 수정하기 - 입력값 에러")
+    @Test
+    void updateProfile_error() throws Exception {
+        String bio = "길게 소개를 수정하는 경우. 길게 소개를 수정하는 경우. 길게 소개를 수정하는 경우. 너무나도 길게 소개를 수정하는 경우. ";
+        mockMvc.perform(post(ROOT + SETTINGS + PROFILE)
+                        .param("bio", bio)
+                        .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(view().name(SETTINGS + PROFILE))
+                .andExpect(model().attributeExists("account"))
+                .andExpect(model().attributeExists("profile"))
+                .andExpect(model().hasErrors());
+
+        Account account = accountRepository.findByNickname("testAccount");
+        assertNull(account.getBio());
+    }
+
+
+    @WithAccount(TEST_ACCOUNT)
+    @DisplayName("패스워드 수정 폼")
+    @Test
+    void updatePassword_form() throws Exception {
+        mockMvc.perform(get(ROOT + SETTINGS + PASSWORD))
+                .andExpect(status().isOk())
+                .andExpect(model().attributeExists("account"))
+                .andExpect(model().attributeExists("passwordForm"))
+        ;
+    }
+
+    @WithAccount(TEST_ACCOUNT)
+    @DisplayName("패스워드 수정 - 입력값 정상 ")
+    @Test
+    void updatePassword_success() throws Exception {
+        mockMvc.perform(post(ROOT + SETTINGS + PASSWORD)
+                .param("newPassword", TEST_PASS)
+                .param("newPasswordConfirm", TEST_PASS)
+                .with(csrf()))
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl(ROOT + SETTINGS + PASSWORD))
+                .andExpect(flash().attributeExists("message"))
+        ;
+
+        Account account = accountRepository.findByNickname(TEST_ACCOUNT);
+        assertTrue(passwordEncoder.matches(TEST_PASS, account.getPassword()));
+    }
+
+    @WithAccount(TEST_ACCOUNT)
+    @DisplayName("패스워드 수정 - 입력값 패스워드 불일치 ")
+    @Test
+    void updatePassword_fail() throws Exception {
+        mockMvc.perform(post(ROOT + SETTINGS + PASSWORD)
+                        .param("newPassword", TEST_PASS)
+                        .param("newPasswordConfirm", "TEST_PASS")
+                        .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(view().name(SETTINGS + PASSWORD))
+                .andExpect(model().hasErrors())
+                .andExpect(model().attributeExists("passwordForm"))
+                .andExpect(model().attributeExists("account"))
+        ;
+    }
+
+
+
+
+
+
+
+
+
+
 
 }
